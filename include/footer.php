119b7a55478fda33a980757329d23fd430d159a6
Gautrot <72071525+Gautrot@users.noreply.github.com>
2021-03-03 09:17:52 +0100

    .

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="text-center my-auto">
+            <span>Projet</span>
+        </div>
+    </div>
+</footer>
+<!-- Fin Footer -->
